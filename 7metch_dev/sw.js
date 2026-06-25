fcf4d74005ab6230ddba0852361850b57be9415d
kumacha666 <danakaya3141@gmail.com>
2026-06-25 14:34:51 +0900

    feat: ヒント機能に通常マッチフォールバック追加 (#157)
    特殊ピースヒント優先、なければ通常マッチをランダムにハイライト表示

--- 7metch_dev/sw.js
+++ 7metch_dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "7metch-dev-v47";
+const CACHE_NAME = "7metch-dev-v48";
 const ASSETS = ["./", "./index.html", "./style.css", "./game.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
